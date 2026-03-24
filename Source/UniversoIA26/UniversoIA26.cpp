8a78320335fa95e9e6be9aad178587179aeb8a7c
juanmacodes <76732302+juanmacodes@users.noreply.github.com>
2026-03-24 20:14:41 +0100

    meta y kitbash

---
+++ Source/UniversoIA26/UniversoIA26.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UniversoIA26.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UniversoIA26, "UniversoIA26" );
